4f05d5e0854f5aa8097d5f4c757e6bf7c1007a12
Paulo Villegas <paulo.vllgs@gmail.com>
2020-10-13 12:55:11 +0200

    2020-10-13 - version 1.2.3     * fix: read README file in setup.py as UTF-8 to avoid installation error

--- setup.py
+++ setup.py
@@ -1,43 +1,40 @@
 # -*- coding: utf-8 -*-
+import io
 from distutils.core import setup, Extension
 
 def get_readme():
-    with open('README.rst', 'rt') as f:
+    with io.open('README.rst', 'rt', encoding='utf-8') as f:
         body = f.read()
-
-        marker1 = '.. image'
-        marker2 = '.. contents'
-
+        #marker1 = '.. image'
+        #marker2 = '.. contents'
         #s = body.index(marker1)
         #e = body.index(marker2)
         #body = body[:s] + body[e:]
-
         return body
 
 
 module = Extension(
-	'pydawg',
-	sources = ['pydawg.c'],
-	define_macros = [
-		('DEBUG', 1),                   # define debug mode
-		('DAWG_PERFECT_HASHING', ''),	# enable perfect hashing
-		('DAWG_UNICODE', ''),		# use unicode
-	],
-	depends = [
-		'DAWG_class.c', 'DAWG_class.h',
-		'DAWGIterator_class.c', 'DAWGIterator_class.h',
-		'dawg.c', 'dawg.h', 'dawg_pickle.c', 'dawg_mph.c',
-		'dawgnode.c', 'dawgcode.h',
-		'slist.h', 'slist.c',
-		'utils.c',
-	]
+    'pydawg',
+    sources=['pydawg.c'],
+    define_macros=[
+        ('DEBUG', 1),                   # define debug mode
+        ('DAWG_PERFECT_HASHING', ''),   # enable perfect hashing
+        ('DAWG_UNICODE', ''),		# use unicode
+    ],
+    depends=[
+        'DAWG_class.c', 'DAWG_class.h',
+        'DAWGIterator_class.c', 'DAWGIterator_class.h',
+        'dawg.c', 'dawg.h', 'dawg_pickle.c', 'dawg_mph.c',
+        'dawgnode.c', 'dawgcode.h',
+        'slist.h', 'slist.c',
+        'utils.c',
+    ]
 )
 
 setup(
-	name                = 'pyDAWG',
-    version             = '1.2.2',
-	ext_modules         = [module],
-
+    name                = 'pyDAWG',
+    version             = '1.2.3',
+    ext_modules         = [module],
     description         = "Directed Acyclic Word Graph (DAWG) allows to store huge strings set in compacted form",
     author              = "Wojciech Muła",
     author_email        = "wojciech_mula@poczta.onet.pl",
@@ -52,7 +49,7 @@ setup(
         "graph",
         "dictionary",
     ],
-    classifiers      = [
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: C",
